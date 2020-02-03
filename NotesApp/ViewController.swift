51c8129784509e53d5b6763b4f1a9ee4132e0378
*Amandeep <ammyboparai@gmail.com>
2020-02-03 00:09:07 -0500

    Initial Commit

---
+++ NotesApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NotesApp
+//
+//  Created by Amandeep Kaur on 2020-02-03.
+//  Copyright © 2020 Amandeep Kaur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
